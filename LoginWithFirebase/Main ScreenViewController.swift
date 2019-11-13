5f6776f764054185eb7eded667969173a93eec49
Madhuri Palanivelu <palsmadhu18@gmail.com>
2019-11-13 10:11:59 -0800

    Update Main ScreenViewController.swift

--- LoginWithFirebase/Main ScreenViewController.swift
+++ LoginWithFirebase/Main ScreenViewController.swift
@@ -39,14 +39,5 @@ class Main_ScreenViewController: UIViewController {
     @IBAction func onCloseButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
